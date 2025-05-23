9013a9f7a725293e96779afe40e464d1ef32f249
Vishal <vishal.gupta@appfire.com>
2025-05-23 17:32:30 +0530

    cdk repo

---
+++ test/aws-cdk-efs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkEfs from '../lib/aws-cdk-efs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-efs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkEfs.AwsCdkEfsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
